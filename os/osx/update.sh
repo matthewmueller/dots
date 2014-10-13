f1cc5ae05743c2da9d45f998dfa94626e7867b43
Matthew Mueller <mattmuelle@gmail.com>
2014-10-13 10:45:58 -0700

    fix homebrew update

--- os/osx/update.sh
+++ os/osx/update.sh
@@ -4,7 +4,7 @@ set -e
 # Check for Homebrew
 if test ! $(which brew); then
   echo "Installing homebrew..."
-  ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 # update brew packages
